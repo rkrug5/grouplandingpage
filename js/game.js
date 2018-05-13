4a686180368b673b6aee6810c601de0169fd8675
rkrug5 <phishermankrug@hotmail.com>
2018-05-13 14:38:55 -0700

    cloning group landing page to try to mess around with styling

---
+++ js/game.js
@@ -0,0 +1,27 @@
+$(function () {
+
+
+
+	//This is RJ's javascript that handles our beautiful landing page
+	const left = document.querySelector('.left');
+	const right = document.querySelector('.right');
+	const container = document.querySelector('.container');
+
+	left.addEventListener('mouseenter', () => {
+		container.classList.add('hover-left');
+	});
+
+	left.addEventListener('mouseleave', () => {
+		container.classList.remove('hover-left');
+	});
+
+	right.addEventListener('mouseenter', () => {
+		container.classList.add('hover-right');
+	});
+
+	right.addEventListener('mouseleave', () => {
+		container.classList.remove('hover-right');
+	});
+
+});
+
